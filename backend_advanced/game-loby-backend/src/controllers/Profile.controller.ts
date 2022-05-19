bff1c545c0fb471c33b8a044b19ec57477d12ddf
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-19 11:10:45 -0400

    upload and download profile picture function

--- backend_advanced/game-loby-backend/src/controllers/Profile.controller.ts
+++ backend_advanced/game-loby-backend/src/controllers/Profile.controller.ts
@@ -5,11 +5,20 @@ import {
   Param,
   ParseUUIDPipe,
   Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ProfileDto } from 'src/dto/profile.dto';
 import { ProfileService } from 'src/services/profile/profile.service';
 import { UseGuards } from '@nestjs/common';
 import { AuthenticationGuard } from 'src/common/auth/authentication.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import path = require('path');
+import { v4 as uuidv4 } from 'uuid';
+import { Observable, of } from 'rxjs';
+import { join } from 'path';
 
 @Controller('profile')
 export class ProfileController {
@@ -25,12 +34,39 @@ export class ProfileController {
   }
 
   @UseGuards(AuthenticationGuard)
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './upload',
+        filename: (req, file, cb) => {
+          const filename: string =
+            path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+          path.parse;
+          const extension: string = path.parse(file.originalname).ext;
+
+          cb(null, `${filename}${extension}`);
+        },
+      }),
+    }),
+  )
   @Post('/updatePic')
-  async updateProfilePic(@Body() body: ProfileDto) {
+  async updateProfilePic(
+    @Body() body: ProfileDto,
+    @UploadedFile() file,
+  ): Promise<any> {
     const update = await this.profileService.updateProfilePic(
       body.userID,
-      body.url,
+      file.filename,
     );
     return update;
+  }
+
+  @UseGuards(AuthenticationGuard)
+  @Get('/profile-image/:imagename')
+  downloadProfilePic(
+    @Param('imagename') imagename,
+    @Res() res,
+  ): Observable<Object> {
+    return of(res.sendFile(join(process.cwd(), '/upload/' + imagename)));
   }
 }
--- backend_advanced/game-loby-backend/src/dto/profile.dto.ts
+++ backend_advanced/game-loby-backend/src/dto/profile.dto.ts
@@ -1,4 +1,3 @@
 export class ProfileDto {
   userID: string;
-  url: string;
 }
