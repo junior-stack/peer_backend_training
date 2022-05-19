a188ee6fc54f183a83cab82ae77e44cc280ddd59
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-19 00:14:26 -0400

    add authentication to other apis

--- backend_advanced/game-loby-backend/src/controllers/Profile.controller.ts
+++ backend_advanced/game-loby-backend/src/controllers/Profile.controller.ts
@@ -8,21 +8,29 @@ import {
 } from '@nestjs/common';
 import { ProfileDto } from 'src/dto/profile.dto';
 import { ProfileService } from 'src/services/profile/profile.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthenticationGuard } from 'src/common/auth/authentication.guard';
 
 @Controller('profile')
 export class ProfileController {
-
   constructor(private readonly profileService: ProfileService) {}
 
+  @UseGuards(AuthenticationGuard)
   @Get('/userProfile/:userId')
-  async getProfilePicture(@Param('userId', new ParseUUIDPipe()) userId: string) {
+  async getProfilePicture(
+    @Param('userId', new ParseUUIDPipe()) userId: string,
+  ) {
     const userurl = await this.profileService.getProfilePicture(userId);
     return userurl;
   }
 
+  @UseGuards(AuthenticationGuard)
   @Post('/updatePic')
   async updateProfilePic(@Body() body: ProfileDto) {
-    const update = await this.profileService.updateProfilePic(body.userID, body.url)
+    const update = await this.profileService.updateProfilePic(
+      body.userID,
+      body.url,
+    );
     return update;
   }
 }
