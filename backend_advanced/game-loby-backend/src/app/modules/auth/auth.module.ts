493898bc8c3fdff9f13f267f7b63318d434f51b9
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-18 23:42:22 -0400

    fix some import problems

--- backend_advanced/game-loby-backend/src/app/modules/auth/auth.module.ts
+++ backend_advanced/game-loby-backend/src/app/modules/auth/auth.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from 'src/controllers/Auth.controller';
 import { AuthService } from 'src/services/auth/auth.service';
-import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PassportModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
   providers: [AuthService],
 })
