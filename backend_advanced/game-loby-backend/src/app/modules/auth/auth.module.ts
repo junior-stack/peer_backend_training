75b823bfd4ff738b114702e633e8952cb7dc71e9
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-18 23:27:44 -0400

    fix some imports for this branch

--- backend_advanced/game-loby-backend/src/app/modules/auth/auth.module.ts
+++ backend_advanced/game-loby-backend/src/app/modules/auth/auth.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from 'src/controllers/Auth.controller';
 import { AuthService } from 'src/services/auth/auth.service';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from 'src/services/auth/local.strategy';
-import { SessionSerializer } from 'src/common/auth/session.serializer';
 
 @Module({
   imports: [PassportModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, SessionSerializer],
+  providers: [AuthService],
 })
 export class AuthModule {}
