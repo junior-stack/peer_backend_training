49485b87dc7c5817b9f5e1420b4ca67420388854
yezhou2 <jaxyz.ye@mail.utoronto.ca>
2022-05-19 22:01:02 -0400

    hash the password

--- backend_advanced/game-loby-backend/src/services/auth/auth.service.ts
+++ backend_advanced/game-loby-backend/src/services/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '.prisma/client';
-
+import bcrypt = require('bcrypt');
 @Injectable()
 export class AuthService {
   private prisma = new PrismaClient();
 
   async login(email: string, password: string) {
     const user = await this.prisma.users.findFirst({ where: { email: email } });
-    if (user && user.password === password) {
+    const result = await bcrypt.compare(password, user.password);
+    if (user && result) {
       const { email, password, ...rest } = user;
       return rest;
     }
